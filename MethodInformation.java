638b79c72b968cd08863a9265cdbaeb18496de7d
kanakodavis <matthiasgusenbauer@gmx.net>
2012-12-09 01:43:53 +0100

    added reflection/runtime usage

--- MethodInformation.java
+++ MethodInformation.java
@@ -1,6 +1,9 @@
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME) //for reflection
 @Target(ElementType.METHOD)
 public @interface MethodInformation {
 	
