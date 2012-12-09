699dd03550923b1bd487917dcf7e5afa7bdcb54f
Morka <Nief@mx.at>
2012-12-09 12:50:24 +0100

    Most important bugfix ever! Not sure if completly correct :P

--- MethodInformation.java
+++ MethodInformation.java
@@ -7,8 +7,8 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 public @interface MethodInformation {
 	
-	String author() default "JESUS";
+	String author() default "MASTER CHIEF";
 	String date() default "30.08.2552";
-	String description() default "Does something fancy!";
+	String description() default "Escape from Reach";
 
 }
