3d7d378cc3d623db7218cb84fe27ae3666fed52d
kanakodavis <matthiasgusenbauer@gmx.net>
2012-12-09 22:09:07 +0100

    getMethods() example

--- Test.java
+++ Test.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Method;
+
 /**
  * @author Matthias Gusenbauer, Wolfgang Hofer, Alexander Neff
  */
@@ -8,6 +10,18 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		//Traktor fendtVarioBitch = new BioTraktor();
+		Method[] ms = BioTraktor.class.getDeclaredMethods();
+		
+		for(Method m : ms){
+			
+			System.out.println(m.toString());
+			
+		}
+		
+		System.out.println(ms.length);
+		System.out.println(ms[0]);
 		
 	}
 }
